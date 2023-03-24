eb9c3558940675da17c48fdd3b32b605a085f371
Your Name <you@example.com>
2023-03-24 22:47:50 +0100

    running

--- deep-autoencoder-on-mnist.py
+++ deep-autoencoder-on-mnist.py
@@ -10,27 +10,21 @@ from tensorflow.keras.datasets import mnist
 input_dim = 28*28
 latent_vec_dim = 2
 
-
-def encoderModel():
-    input_layer = Input(shape=(input_dim,))
-    encoder_layer_1 = Dense(400, activation='sigmoid')(input_layer)
-    encoder_layer_2 = Dense(100, activation='sigmoid')(encoder_layer_1)
-    encoder_layer_3 = Dense(10, activation='sigmoid')(encoder_layer_2)
-    encoder_layer_4 = Dense(latent_vec_dim, activation='tanh')(encoder_layer_3)
-    return Model(input_layer, encoder_layer_4, name="encoder")
-
-def decoderModel():
-    input_layer = Input(shape=(latent_vec_dim,))
-    decoder_layer_1 = Dense(10, activation='sigmoid')(input_layer)
-    decoder_layer_2 = Dense(100, activation='sigmoid')(decoder_layer_1)
-    decoder_layer_3 = Dense(400, activation='sigmoid')(decoder_layer_2)
-    decoder_layer_4 = Dense(input_dim, activation='sigmoid')(decoder_layer_3)
-    return Model(input_layer, decoder_layer_4, name="decoder")
-
 input_layer = Input(shape=(input_dim,))
-encoder = encoderModel()(input_layer)
-decoder = decoderModel()(encoder)
-autoencoder = Model(input_layer, decoder, name="autoencoder")
+encoder_layer_1 = Dense(400, activation='sigmoid')(input_layer)
+encoder_layer_2 = Dense(100, activation='sigmoid')(encoder_layer_1)
+encoder_layer_3 = Dense(10, activation='sigmoid')(encoder_layer_2)
+encoder_layer_4 = Dense(latent_vec_dim, activation='tanh')(encoder_layer_3)
+decoder_layer_1 = Dense(10, activation='sigmoid')(encoder_layer_4)
+decoder_layer_2 = Dense(100, activation='sigmoid')(decoder_layer_1)
+decoder_layer_3 = Dense(400, activation='sigmoid')(decoder_layer_2)
+decoder_layer_4 = Dense(input_dim, activation='sigmoid')(decoder_layer_3)
+
+autoencoder = Model(input_layer, decoder_layer_4, name='autoencoder')
+encoder = Model(input_layer, encoder_layer_4, name='encoder')
+
+latent_input = Input(shape=(latent_vec_dim,))
+decoder = Model(latent_input, autoencoder.layers[-4](latent_input), name='decoder')
 
 (train_samples, train_responses), (test_samples, test_responses) = mnist.load_data()
 
@@ -42,7 +36,7 @@ test_samples = np.reshape(test_samples, (-1, 784))
 
 autoencoder.compile(loss='binary_crossentropy', optimizer='adam')
 
-history = autoencoder.fit(train_samples, train_samples, epochs=10, batch_size=128,
+history = autoencoder.fit(train_samples, train_samples, epochs=100, batch_size=128,
                           shuffle=True, validation_data=(test_samples, test_samples))
 
 plt.plot(history.history['loss'], label='Train')
@@ -55,7 +49,6 @@ plt.savefig('loss.svg')
 
 latent_predictions = encoder(train_samples)
 
-# pl = np.concatenate(latent_predictions, train_responses, axis=1)
 pl = tf.concat([latent_predictions, tf.expand_dims(tf.cast(train_responses, dtype=tf.float32), axis=1)], 1)
 npl = pl.numpy()
 
@@ -65,6 +58,26 @@ plt.colorbar()
 plt.savefig('scatter.svg')
 savetxt('pl.csv', pl)
 
- encoder.save_weights('./ae/ae_enc_400_100_10_2')
+encoder.save_weights('./ae/ae_enc_400_100_10_2')
 decoder.save_weights('./ae/ae_dec_400_100_10_2')
 
+test_imgs = autoencoder.predict(test_samples)
+
+
+n = 10  # How many digits we will display
+plt.figure(figsize=(20, 4))
+for i in range(n):
+    # Display original
+    ax = plt.subplot(2, n, i + 1)
+    plt.imshow(test_samples[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # Display reconstruction
+    ax = plt.subplot(2, n, i + 1 + n)
+    plt.imshow(test_imgs[i].reshape(28, 28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+plt.savefig('samples.png')
