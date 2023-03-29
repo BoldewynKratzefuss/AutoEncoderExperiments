d1b917fad8631c83c57275182bd0258a1617b5ef
John Doe <johndoe@example.com>
2023-03-29 18:03:58 +0200

    with graphics

---
+++ TrainLog.py
@@ -0,0 +1,92 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import imageio
+import numpy as np
+from tensorflow.python.ops.numpy_ops import np_config
+import AutoEncoder
+
+class TrainLog(tf.keras.callbacks.Callback):
+    def __init__(self, autoencoder, train_samples, train_responses):
+        np_config.enable_numpy_behavior()
+        
+        self.loop = 0
+        self.train_samples = train_samples
+        self.train_responses = train_responses
+        self.autoencoder = autoencoder
+        pass
+
+    def map_latent(self):
+        latent = self.autoencoder.encode(self.train_samples)
+
+        pl = tf.concat([latent, tf.expand_dims(tf.cast(self.train_responses, dtype=tf.float32), axis=1)], 1)
+        npl = pl.numpy()
+
+        plt.clf()
+        plt.cla()
+        plt.figure(figsize=(5, 5))
+        plt.scatter(npl[:,0],npl[:,1],s=20, c=npl[:,2], cmap='tab10')
+        plt.colorbar()
+        plt.savefig("./out/latent{x}.svg".format(x=self.loop))
+        plt.savefig("./out/latent{x}.png".format(x=self.loop), dpi=96)
+
+    def predict_examples(self):
+        test_imgs = self.autoencoder.predict(self.train_samples)
+
+        n = 10  # How many digits we will display
+        plt.clf()
+        plt.cla()
+        plt.figure(figsize=(20, 4))
+        for i in range(n):
+            # Display original
+            ax = plt.subplot(2, n, i + 1)
+            plt.imshow(self.train_samples[i].reshape(28, 28))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+            # Display reconstruction
+            ax = plt.subplot(2, n, i + 1 + n)
+            plt.imshow(test_imgs[i].reshape(28, 28))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+        plt.savefig("./out/samples{x}.png".format(x=self.loop), dpi=96)
+
+    def map_encoder(self):
+        n = 10
+        r = np.linspace(-1, 1, n)
+        l = np.meshgrid(r, r)
+        l = np.reshape(l, [100, 2])
+
+        imgs = self.autoencoder.decode(l)
+        plt.clf()
+        plt.cla()
+        plt.figure(figsize=(20, 20))
+        x = 0
+        for r in range(n):
+            for c in range(n):
+                ax = plt.subplot(n, n, x + 1)
+                plt.imshow(imgs[x].reshape(28, 28))
+                plt.gray()
+                ax.get_xaxis().set_visible(False)
+                ax.get_yaxis().set_visible(False)
+                x = x+1
+
+        plt.savefig("./out/dec_map{x}.png".format(x=self.loop), dpi=96)
+        pass
+
+    def on_epoch_end(self, epoch, logs=None):
+        self.map_latent()
+        self.predict_examples()
+        self.map_encoder()
+        self.loop = self.loop+1
+
+    def pack_gif(self, pattern_in_x, out, count):
+        images = []
+        frame_length = 0.5 # seconds between frames
+        end_pause = 4 # seconds to stay on last frame
+        # loop through files, join them to image array, and write to GIF called 'wind_turbine_dist.gif'
+        for ii in range(0, count):       
+            images.append(imageio.imread(pattern_in_x.format(x=ii)))
+        # the duration is the time spent on each image (1/duration is frame rate)
+        imageio.mimsave(out, images,'GIF',duration=frame_length)
--- deep-autoencoder-on-mnist.py
+++ deep-autoencoder-on-mnist.py
@@ -7,24 +7,16 @@ from tensorflow.keras import Model, Input
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.datasets import mnist
 
+import TrainLog as tl
+import AutoEncoder as ae
+
+epochs_count = 100
+batch_size = 128
+
 input_dim = 28*28
 latent_vec_dim = 2
 
-input_layer = Input(shape=(input_dim,))
-encoder_layer_1 = Dense(400, activation='sigmoid')(input_layer)
-encoder_layer_2 = Dense(100, activation='sigmoid')(encoder_layer_1)
-encoder_layer_3 = Dense(10, activation='sigmoid')(encoder_layer_2)
-encoder_layer_4 = Dense(latent_vec_dim, activation='tanh')(encoder_layer_3)
-decoder_layer_1 = Dense(10, activation='sigmoid')(encoder_layer_4)
-decoder_layer_2 = Dense(100, activation='sigmoid')(decoder_layer_1)
-decoder_layer_3 = Dense(400, activation='sigmoid')(decoder_layer_2)
-decoder_layer_4 = Dense(input_dim, activation='sigmoid')(decoder_layer_3)
-
-autoencoder = Model(input_layer, decoder_layer_4, name='autoencoder')
-encoder = Model(input_layer, encoder_layer_4, name='encoder')
-
-latent_input = Input(shape=(latent_vec_dim,))
-decoder = Model(latent_input, autoencoder.layers[-4](latent_input), name='decoder')
+autoencoder = ae.AutoEncoder(input_dim, latent_vec_dim)
 
 (train_samples, train_responses), (test_samples, test_responses) = mnist.load_data()
 
@@ -36,48 +28,24 @@ test_samples = np.reshape(test_samples, (-1, 784))
 
 autoencoder.compile(loss='binary_crossentropy', optimizer='adam')
 
-history = autoencoder.fit(train_samples, train_samples, epochs=100, batch_size=128,
-                          shuffle=True, validation_data=(test_samples, test_samples))
 
-plt.plot(history.history['loss'], label='Train')
-plt.plot(history.history['val_loss'], label='Validation')
-plt.ylabel('Cross Entropy Loss')
-plt.xlabel('Epochs')
-plt.title('Learning Loss Curve', pad=13)
-plt.legend(loc='upper right')
-plt.savefig('loss.svg')
+cb = tl.TrainLog(autoencoder, train_samples, train_responses)
+# history = autoencoder.fit(train_samples, train_samples, epochs=epochs_count, batch_size=batch_size,
+#                           shuffle=True, validation_data=(test_samples, test_samples),
+#                           callbacks=[cb])
 
-latent_predictions = encoder(train_samples)
+# plt.clf()
+# plt.cla()
+# plt.figure(figsize=(6, 6))
+# plt.plot(history.history['loss'], label='Train')
+# plt.plot(history.history['val_loss'], label='Validation')
+# plt.ylabel('Cross Entropy Loss')
+# plt.xlabel('Epochs')
+# plt.title('Learning Loss Curve', pad=13)
+# plt.legend(loc='upper right')
+# plt.savefig('./out/loss.svg')
 
-pl = tf.concat([latent_predictions, tf.expand_dims(tf.cast(train_responses, dtype=tf.float32), axis=1)], 1)
-npl = pl.numpy()
+cb.pack_gif('./out/dec_map{x}.png', './out/dec_map.gif', epochs_count)
+cb.pack_gif('./out/latent{x}.png', './out/latent.gif', epochs_count)
+cb.pack_gif('./out/samples{x}.png', './out/samples.gif', epochs_count)
 
-plt.clf()
-plt.scatter(npl[:,0],npl[:,1],s=20, c=npl[:,2], cmap='tab10')
-plt.colorbar()
-plt.savefig('scatter.svg')
-savetxt('pl.csv', pl)
-
-encoder.save_weights('./ae/ae_enc_400_100_10_2')
-decoder.save_weights('./ae/ae_dec_400_100_10_2')
-
-test_imgs = autoencoder.predict(test_samples)
-
-
-n = 10  # How many digits we will display
-plt.figure(figsize=(20, 4))
-for i in range(n):
-    # Display original
-    ax = plt.subplot(2, n, i + 1)
-    plt.imshow(test_samples[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
-    # Display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
-    plt.imshow(test_imgs[i].reshape(28, 28))
-    plt.gray()
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-plt.savefig('samples.png')
